8ae9767a9d9ca140f87473d2b817da65626e66df
hadeelabbadi <hadeelabbadi03@gmail.com>
2026-04-28 00:43:57 +0300

    Task 19

--- App.tsx
+++ App.tsx
@@ -1,11 +1,3 @@
-// import Task16 from './Tasks/Task16';
-// import Task17 from './Tasks/Task17';
-// import Task18 from './Tasks/Task18';
-
-/*export default function App() {
-  return <Task18 />;
-}*/
-
 import Task19 from './Tasks/Task19';
 
 export default function App() {
--- Tasks/Task19.js
+++ Tasks/Task19.js
@@ -1,5 +1,45 @@
-import Task19 from './Tasks/Task19';
+import React, { Component } from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 
-export default function App() {
-  return <Task19 />;
+class Task19 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.loading ? (
+          <>
+            <ActivityIndicator size="large" />
+            <Text>Loading...</Text>
+          </>
+        ) : (
+          <Text style={styles.text}>Hadeel</Text>
+        )}
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default Task19;
