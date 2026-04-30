740fb20d58c0a989610707461e5946128d9b77f6
hadeelabbadi <hadeelabbadi03@gmail.com>
2026-04-30 12:54:24 +0300

    Task 35 - Styled list items

--- Tasks/Screen1.js
+++ Tasks/Screen1.js
@@ -1,4 +1,10 @@
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 
 export default function Screen1({ navigation }) {
   const users = [
@@ -9,22 +15,39 @@ export default function Screen1({ navigation }) {
   ];
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={users}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity
+            style={styles.card}
             onPress={() =>
               navigation.navigate('Screen2', { name: item.name })
             }
           >
-            <Text style={{ fontSize: 20, margin: 10 }}>
-              {item.name}
-            </Text>
+            <Text style={styles.text}>{item.name}</Text>
           </TouchableOpacity>
         )}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  card: {
+  backgroundColor: '#4CAF50',
+  padding: 20,
+  marginVertical: 10,
+  borderRadius: 10,
+  elevation: 5,
+},
+  text: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
