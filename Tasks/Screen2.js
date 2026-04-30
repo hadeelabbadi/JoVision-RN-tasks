ad58ad81a879e9021ede00485b72554a7820d680
hadeelabbadi <hadeelabbadi03@gmail.com>
2026-04-30 12:32:52 +0300

    Task 31 - Dynamic header title

--- Tasks/Screen2.js
+++ Tasks/Screen2.js
@@ -1,7 +1,14 @@
 import { View, Text, Button } from 'react-native';
+import { useEffect } from 'react';
 
 export default function Screen2({ route, navigation }) {
   const name = route.params?.name || 'Guest';
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: `Welcome ${name}`,
+    });
+  }, []);
 
   return (
     <View>
