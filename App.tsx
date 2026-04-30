2e0ed2c2261c845d89b737aa7530b43e7d077c23
hadeelabbadi <hadeelabbadi03@gmail.com>
2026-04-30 12:00:08 +0300

    Task 27 - Navigation working

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
-import Task26 from './Tasks/Task26';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Screen1 from './Tasks/Screen1';
+import Screen2 from './Tasks/Screen2';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Task26 />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen1" component={Screen1} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
